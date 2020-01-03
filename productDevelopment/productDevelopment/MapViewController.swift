5a8e9db265742ee0f9c1a6ae7b2cdb0b89082fcf
hchowdhury3273 <hc2324@nyu.edu>
2020-01-03 18:58:27 -0500

    pods

---
+++ productDevelopment/productDevelopment/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  productDevelopment
+//
+//  Created by Helal Chowdhury on 1/3/20.
+//  Copyright © 2020 Helal. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import Speech
+import FlyoverKit
+
+
+class MapViewController: UIViewController, MKMapViewDelegate, SFSpeechRecognizerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
